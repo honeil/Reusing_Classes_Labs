9a4d93e72a5cec9f8600fa7998a829c106d85e2a
Hilary O'Neil <hilarya.oneil@gmail.com>
2017-01-19 08:56:10 -0500

    added rotated array loop

--- src/main/java/oneil/hilary/zipcode/LabOneRotateArray.java
+++ src/main/java/oneil/hilary/zipcode/LabOneRotateArray.java
@@ -1,8 +1,6 @@
 package oneil.hilary.zipcode;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.*;
+import java.util.Arrays;
 
 /**
  * Created by hilaryoneil on 1/18/17.
@@ -11,12 +9,18 @@ public class LabOneRotateArray {
 
 
     public static void main(String [] args){
-        List<Integer> myArray = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        int[] myArray = new int[]{1,2,3,4,5,6,7};
+        int k = 2;
+        System.out.println("Input: " + Arrays.toString(myArray));
 
-        for(Integer x: myArray) {
-            System.out.println(x);
+        for(int i = 0; i < k; i++) {
+            for (int j = 0; j < myArray.length - 1; j++) {
+                int x = myArray[j];
+                myArray[j] = myArray[j + 1];
+                myArray[j + 1]= x;
+                }
         }
-
-    }
+        System.out.println("Output:" + Arrays.toString(myArray));
+   }
 }
 

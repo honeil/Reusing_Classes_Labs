5a12dade7c31e594d0a8c7c82743b0ab8d76a3b5
Hilary O'Neil <hilarya.oneil@gmail.com>
2017-01-19 00:33:15 -0500

    created an array

---
+++ src/main/java/oneil/hilary/zipcode/LabOneRotateArray.java
@@ -0,0 +1,22 @@
+package oneil.hilary.zipcode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
+
+/**
+ * Created by hilaryoneil on 1/18/17.
+ */
+public class LabOneRotateArray {
+
+
+    public static void main(String [] args){
+        List<Integer> myArray = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        for(Integer x: myArray) {
+            System.out.println(x);
+        }
+
+    }
+}
+
